bcb975ef1fbc22936d34067925e3cb02f7a8fc97
Magnus Bergmark <magnus.bergmark@gmail.com>
2015-09-04 23:00:46 +0200

    Use Puma as Ruby web server; remove logs

--- ruby/server.rb
+++ ruby/server.rb
@@ -2,42 +2,52 @@ require "json"
 require "bundler/setup"
 require "sinatra"
 
-set :port, ENV.fetch("PORT", 3000)
+class App < Sinatra::Base
+  port = ENV.fetch("PORT", 3000)
+  puts "Starting server on port #{port}"
 
-def time_text
-  time = Time.now
-  (time.min >= 30 ? "half past #{time.hour}" : "#{time.hour} O'clock")
+  set :port, port
+  set :logging, false
+
+  def time_text
+    time = Time.now
+    (time.min >= 30 ? "half past #{time.hour}" : "#{time.hour} O'clock")
+  end
+
+  def time_json
+    time = Time.now
+    {
+      stamp: time.to_i,
+      fullstamp: time.to_f,
+      string: time.iso8601,
+    }.to_json
+  end
+
+  get "/api/current-time", provides: "text" do
+    mime_type :text
+    time_text
+  end
+
+  get "/api/current-time", provides: "json" do
+    mime_type :json
+    time_json
+  end
+
+  get "/api/current-time.json" do
+    mime_type :json
+    time_json
+  end
+
+  get "/api/current-time.txt" do
+    mime_type :text
+    time_text
+  end
+
+  get "*" do
+    [404, ["Not Found".freeze]]
+  end
 end
 
-def time_json
-  time = Time.now
-  {
-    stamp: time.to_i,
-    fullstamp: time.to_f,
-    string: time.iso8601,
-  }.to_json
-end
-
-get "/api/current-time", provides: "text" do
-  mime_type :text
-  time_text
-end
-
-get "/api/current-time", provides: "json" do
-  mime_type :json
-  time_json
-end
-
-get "/api/current-time.json" do
-  mime_type :json
-  time_json
-end
-
-get "/api/current-time.txt" do
-  mime_type :text
-  time_text
-end
-
-get "*" do
-  [404, ["Not Found".freeze]]
+if __FILE__ == $0
+  App.run!
 end
